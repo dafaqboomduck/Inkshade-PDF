d5a62ffebc452ecc8b2bf2e87b233532ab057977
dafaqboomduck <rzvn1805@gmail.com>
2026-02-03 12:05:07 +0200

    chore: Update widgets exports with backwards compatibility

--- ui/widgets/__init__.py
+++ ui/widgets/__init__.py
@@ -1,8 +1,13 @@
 """
 Custom widgets for PDF viewing and interaction.
 """
-from .page_label import ClickablePageLabel
+
+from .page_label import InteractivePageLabel
 from .pdf_viewer import PDFViewer
 from .toc_widget import TOCWidget
 
-__all__ = ['ClickablePageLabel', 'PDFViewer', 'TOCWidget']
+__all__ = [
+    "InteractivePageLabel",
+    "PDFViewer",
+    "TOCWidget",
+]
